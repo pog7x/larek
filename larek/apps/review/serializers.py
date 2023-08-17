0f6f9c10c860ed36dc36faac1e0d701dbc1d6f3e
pog7x <poluningm@gmail.com>
2023-08-17 12:51:20 +0300

    review created_at

--- larek/apps/review/serializers.py
+++ larek/apps/review/serializers.py
@@ -8,7 +8,8 @@ class ReviewSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField()
     product_id = serializers.IntegerField()
     user = UserSerializer(required=False)
+    created_at = serializers.DateTimeField(required=False, format="%d %B %Y %H:%M")
 
     class Meta:
         model = Review
-        fields = ["user_id", "product_id", "user", "comment"]
+        fields = ["user_id", "product_id", "user", "comment", "created_at"]
