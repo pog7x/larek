ccd2ae981e3924cc94a38422c732fb5310828086
pog7x <poluningm@gmail.com>
2024-07-07 10:41:12 +0300

    clear migrations fix path

--- clear_migrations.py
+++ clear_migrations.py
@@ -6,6 +6,6 @@ for name, folders, files in os.walk(os.curdir):
     if name.endswith("migrations"):
         for file in files:
             if file != "__init__.py":
-                migr_path = f"{name}/{file}"
+                migr_path = os.path.join(name, file)
                 print("Cleaning up migration:", migr_path)
                 os.remove(migr_path)
